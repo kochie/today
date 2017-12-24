84227ed0d07a6d06ac485c05d8f67bcf04c0b5d8
Robert Koch <robert@kochie.io>
2017-12-24 23:09:36 +1100

    Updated the site with several upgrades.
    - Removed the Dockerfile as it is no longer needed for deployment.
    - Added a deploy script to the npm package.
    - Added styling for the site in the html.
    - Added a new route in the server to retrieve all quotes.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const sendTheWords = require('./sendTheWords');
+const sendAllTheWords = require('./sendAllTheWords')
 
 const app = express();
 const PORT_NUMBER = 3000;
@@ -10,5 +11,7 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/api/v1/today', sendTheWords)
+
+app.get('/api/v1/all', sendAllTheWords)
 
 app.listen(PORT_NUMBER, ()=>{console.log(`App is running on port ${PORT_NUMBER}`)})
